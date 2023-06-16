9e8273a1e661710e77711394da2fa5c86bcf6c6f
Raghav Sarangi <raghusarangi4@gmail.com>
2023-06-16 13:27:32 -0400

    checking database 2.4

--- index.php
+++ index.php
@@ -1,20 +1,23 @@
 <?php
 
 echo "Hello World!" ; 
-echo 'ur mum';
 
 
-// //Get Heroku ClearDB connection information
-// $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-// $cleardb_server = $cleardb_url["host"];
-// $cleardb_username = $cleardb_url["user"];
-// $cleardb_password = $cleardb_url["pass"];
-// $cleardb_db = substr($cleardb_url["path"],1);
-// $active_group = 'default';
-// $query_builder = TRUE;
-// // Connect to DB
-// $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
-echo "Client library version: %s\n", mysqli_get_client_info()
+$db_list = mysqli_query($conn, "SHOW DATABASES");
+
+while ($row = mysql_fetch_object($db_list)) {
+ echo $row->Database . "\n";
+}  
 
 ?>
