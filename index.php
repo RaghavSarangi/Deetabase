7a9f6237cda2d75bfbba4102cb9652c676bf03d6
Raghav Sarangi <raghusarangi4@gmail.com>
2023-06-30 00:07:10 -0400

    changed the way of accessing the clearDB database

--- index.php
+++ index.php
@@ -18,35 +18,36 @@ Don't have an account? <a href="signup.php">Sign up now.</a><br/>
 </div>
 <br/>
 
-<p align="right">
-Author: Souvik Das <br/>
-Purdue University, 2021 <br/>
-souvik@purdue.edu
-</p>
-
 
 <?php
 
 //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
+// Read the JSON file 
+$json = file_get_contents('./databaseURL.json');
+  
+// Decode the JSON file
+$json_data = json_decode($json,true);
+  
+// Display data
+// print_r($json_data);
+
+$cleardb_server = $json_data["Host"];
+$cleardb_username = $json_data["Username"];
+$cleardb_password = $json_data["Password"];
+$cleardb_db = $json_data["Database"];
+
+
 // Connect to DB
 $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
-if ($mysqli -> connect_errno) {
+if ($conn -> connect_errno) {
   echo("Failed to connect to MySQL: " . $mysqli -> connect_error);
   exit();
 }
 else {echo($conn->host_info . "\n");}
 
-
-
+// $db_list = mysqli_query($conn, "SHOW DATABASES");
 // while ($obj = mysqli_fetch_object($db_list)) {
 //     echo("<br>" . $obj->Database);
 // }
--- signup.php
+++ signup.php
@@ -20,15 +20,16 @@
   <b>Affiliation</b><br/>
     <select name="affiliation">
       <?php
-        $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-        $cleardb_server = $cleardb_url["host"];
-        $cleardb_username = $cleardb_url["user"];
-        $cleardb_password = $cleardb_url["pass"];
-        $cleardb_db = substr($cleardb_url["path"],1);
-        $active_group = 'default';
-        $query_builder = TRUE;
         // Connect to DB
+        $json = file_get_contents('./databaseURL.json');
+        // Decode the JSON file
+        $json_data = json_decode($json,true);
+        $cleardb_server = $json_data["Host"];
+        $cleardb_username = $json_data["Username"];
+        $cleardb_password = $json_data["Password"];
+        $cleardb_db = $json_data["Database"];
         $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
 
         $sqlQuery = "SELECT affiliation FROM affiliations";
         $queryResult = mysqli_query($conn, $sqlQuery);
