bd81b278b55352debaa120f0e70a4fbdd3056305
Raghav Sarangi <raghusarangi4@gmail.com>
2023-07-18 05:19:21 -0400

    updated adding feature

--- main.php
+++ main.php
@@ -20,6 +20,7 @@
     echo "<ul> \n";
     if ($_SESSION["privilege"] == "Editor" || $_SESSION["privilege"] == "Administrator") echo "<li><a href='add_dee.php'> <b>Add</b> Dees to the database </a></li> \n";
     echo "<li><a href='searchpart.php'> <b>Search</b> for and <b>change geographical location</b> of sheets in the database </a></li> \n";
+    echo "<li><a href='search_all.php'> <b>See all Dees</b>  recorded in the database </a></li> \n";
     echo "<li><a href='users.php'> View <b>users</b> of the database </a></li> \n";
     echo "<li><a href='logout.php'> <b>Logout</b> </a></li> \n";
     echo "</ul> \n";
---
+++ search_all.php
@@ -0,0 +1,72 @@
+<html>
+<head>
+  <title>CMS Tracker Flat Sheets Database</title>
+  <link rel="StyleSheet" type="text/css" href="theme.css"/>
+</head>
+<body>
+
+<script src="showHideElements.js"></script>
+
+<h1 align="center"> CMS Tracker Flat Sheets Database </h1>
+
+<?php
+  session_start();
+  if ($_SESSION["username"] != "")
+  {
+    echo "Current User: ".$_SESSION["firstname"]." ".$_SESSION["lastname"]."<br/> \n";
+    echo "Affiliation: ".$_SESSION["affiliation"]."<br/> \n";
+    echo "Privilege: ".$_SESSION["privilege"]."<br/> \n";
+
+    echo "<br/> \n";
+    echo "<p> \n";
+    echo "<h2> Dees Produced </h2> \n";
+    echo "<table> \n";
+    echo " <tr> \n";
+    echo "  <th> ID </th> \n";
+    echo "  <th> Operators </th> \n";
+    echo "  <th> Manufacturing Datetime </th> \n";
+    echo "  <th> Carbon Foam Serial Number </th> \n";
+    echo "  <th> X-Ray Evaluation </th> \n";
+    echo " </tr> \n";
+
+    $json = file_get_contents('./databaseURL.json');
+    $json_data = json_decode($json,true);
+    $cleardb_server = $json_data["Host"];
+    $cleardb_username = $json_data["Username"];
+    $cleardb_password = $json_data["Password"];
+    $cleardb_db = $json_data["Database"];
+    $connection = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+    $sqlQuery = "SELECT id, operators, manufacture_datetime, carbon_foam_serial_num, xray_evaluation FROM dees";
+    $queryResult = mysqli_query($connection, $sqlQuery);
+    while ($map_output = mysqli_fetch_assoc($queryResult))
+    {
+      echo "<tr> \n";
+    //   echo " <td> ".$map_output["id"]." ".$map_output["operators"]." </td> \n";
+      echo " <td> ".$map_output["id"]." </td> \n";
+      echo " <td> ".$map_output["operators"]." </td> \n";
+      echo " <td> ".$map_output["manufacture_datetime"]." </td> \n";
+      echo " <td> ".$map_output["carbon_foam_serial_num"]." </td> \n";
+      echo " <td> ".$map_output["xray_evaluation"]." </td> \n";
+      echo "</tr> \n";
+    }
+    mysqli_close($connection);
+
+    echo "</table> \n";
+
+      }
+?>
+
+
+<br/><br/>
+<a href="main.php">Back</a>
+<br/>
+
+<p align="right">
+Author: Raghav Sarangi <br/>
+Cornell University, 2023 <br/>
+rs977@cornell.edu
+</p>
+
+</body>
+</html>
